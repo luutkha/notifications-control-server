58508ddfb1b083f22b60db52f3748477408ab464
khatl.fe <luutkha@gmail.com>
2024-01-28 16:33:59 +0700

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -29,7 +29,7 @@ app.use(compress());
 app.use(methodOverride());
 
 // secure apps by setting various HTTP headers
-app.use(helmet());
+// app.use(helmet());
 
 // app.use(
 //   helmet.contentSecurityPolicy({
